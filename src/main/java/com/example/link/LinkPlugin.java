f6c7c289bc30cebd2e6f9735017012bf70b677a6
havardpede <havardpede@gmail.com>
2026-03-24 23:26:32 +0100

    fix: switch from ChatMessage to CommandExecuted for :: commands
    CommandExecuted is in net.runelite.api.events and fires for :: prefixed
    commands. ChatMessage/PUBLICCHAT does not receive :: commands.

--- src/main/java/com/example/link/LinkPlugin.java
+++ src/main/java/com/example/link/LinkPlugin.java
@@ -2,11 +2,10 @@ package com.example.link;
 
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.events.ChatMessage;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.api.events.CommandExecuted;
 import net.runelite.client.party.PartyService;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
@@ -16,7 +15,7 @@ import com.google.inject.Provides;
 @PluginDescriptor(name = "Link")
 public class LinkPlugin extends Plugin
 {
-	private static final String SPIKE_COMMAND = "::linktest";
+	private static final String SPIKE_COMMAND = "linktest";
 
 	@Inject
 	private Client client;
@@ -40,24 +39,17 @@ public class LinkPlugin extends Plugin
 	}
 
 	@Subscribe
-	public void onChatMessage(ChatMessage event)
+	public void onCommandExecuted(CommandExecuted event)
 	{
-		if (event.getType() != ChatMessageType.PUBLICCHAT
-			&& event.getType() != ChatMessageType.MODCHAT)
+		if (!event.getCommand().equals(SPIKE_COMMAND))
 		{
 			return;
 		}
 
-		String message = event.getMessage();
-		if (!message.startsWith(SPIKE_COMMAND))
+		String[] args = event.getArguments();
+		if (args.length > 0)
 		{
-			return;
-		}
-
-		String[] parts = message.split(" ", 2);
-		if (parts.length > 1)
-		{
-			String passphrase = parts[1].trim();
+			String passphrase = String.join(" ", args);
 			log.info("Spike: joining party with passphrase '{}'", passphrase);
 			partyService.changeParty(passphrase);
 		}
